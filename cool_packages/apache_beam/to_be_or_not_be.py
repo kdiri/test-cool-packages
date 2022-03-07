0bbb9b99984dd95427295c7ea35aca5360a3cef9
Kemal Diri <kemaldiri@gmail.com>
2022-03-07 12:21:28 +0100

    Add second count example :owl:

---
+++ cool_packages/apache_beam/to_be_or_not_be.py
@@ -0,0 +1,33 @@
+"""
+    ..synopsis::
+        This module contains the functions to be used in the Apache Beam pipeline.
+"""
+
+import apache_beam as beam
+import apache_beam.transforms.window as window
+from apache_beam.options.pipeline_options import PipelineOptions
+
+
+def run_pipeline():
+    options = PipelineOptions()
+
+    with beam.Pipeline(options=options) as p:
+        (p
+         # Load some dummy data, this can be replaced with a proper source later on
+         | "Create words" >> beam.Create(["to be or not to be that is the question or be hungry be foolish"])
+         # Split the words into one element per word
+         | "Split words" >> beam.FlatMap(lambda x: x.split(" "))
+         # We are assigning a count of 1 to every word (very relevant if we had more data)
+         | "Pair with 1" >> beam.Map(lambda x: (x, 1))
+         # We are interested in 10 second periods of words
+         | "Window of 10 seconds" >> beam.WindowInto(window.FixedWindows(10))
+         # Group all the values (counts) of each unique word
+         | "Group by key" >> beam.GroupByKey()
+         # Sum the counts for each word and return the result
+         | "Sum word counts" >> beam.Map(lambda kv: (kv[0], sum(kv[1])))
+         # Just print to the console for testing
+         | "Print to console" >> beam.Map(lambda wordcount: print(wordcount))
+        )
+
+if __name__ == "__main__":
+    run_pipeline()
